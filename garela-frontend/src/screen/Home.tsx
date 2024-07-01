91462b7311b40c3cfd4fb1800a74ccb351a1c287
Yeonshin <yeonnim05@gmail.com>
2024-07-02 00:32:55 +0900

    (#8) Footer 추가

--- garela-frontend/src/screen/Home.tsx
+++ garela-frontend/src/screen/Home.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Home: React.FC = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
