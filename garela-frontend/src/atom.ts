51e9be74173eb864917f7c33388cea1822b6c526
Yeonshin <yeonnim05@gmail.com>
2024-07-01 17:37:49 +0900

    (#4) 기본 유저정보 인터페이스 Set

--- garela-frontend/src/atom.ts
+++ garela-frontend/src/atom.ts
@@ -1,1 +1,18 @@
 import { atom } from "recoil";
+
+export interface UserInfoType {
+  email: string;
+  photo: string | null;
+  userId: number;
+  name: string;
+}
+
+export const userInfoState = atom<UserInfoType>({
+  key: "userInfoState",
+  default: {
+    email: "",
+    photo: null,
+    userId: 0,
+    name: ""
+  }
+});
