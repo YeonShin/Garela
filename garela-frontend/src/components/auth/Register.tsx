3ae64964668cd5e46070df0cdef22083665d0f4b
Yeonshin <yeonnim05@gmail.com>
2024-07-04 00:00:10 +0900

    (#31) 회원가입 api 연결

--- garela-frontend/src/components/auth/Register.tsx
+++ garela-frontend/src/components/auth/Register.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { UserInfoType, userInfoState } from "../../atom";
@@ -11,7 +12,6 @@ const OuterContainer = styled.div`
   justify-content: center;
   min-height: 90vh;
   background-color: ${(props) => props.theme.colors.background};
-
 `;
 
 const InnerContainer = styled.div`
@@ -69,10 +69,13 @@ const Label = styled.label`
   color: #333;
 `;
 
-const Input = styled.input`
+const Input = styled.input<{ isChecked: boolean | null; isError?: boolean }>`
   padding: 0.5rem;
   font-size: 1rem;
-  border: 1px solid #ddd;
+  border: 1px solid
+    ${(props) =>
+      props.isChecked === null ? "#ddd" : props.isChecked ? "green" : "red"};
+  border-color: ${(props) => (props.isError ? "red" : "")};
   border-radius: 5px;
 `;
 
@@ -98,6 +101,11 @@ const Button = styled.button`
   &:hover {
     background-color: #483d8b;
   }
+
+  &:disabled {
+    background-color: #ddd;
+    cursor: not-allowed;
+  }
 `;
 
 const ProgressBar = styled.div`
@@ -115,14 +123,25 @@ const ProgressStep = styled.div<{ active: boolean }>`
   border-radius: 5px;
 `;
 
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 0.9rem;
+`;
+
 interface FormData {
   email: string;
   password: string;
   passwordCheck: string;
-  phone: string;
   name: string;
   information: string;
 }
+
+interface UserInfo {
+  email: string;
+  password: string;
+  name: string;
+  info : string;
+};
 
 const Register: React.FC = () => {
   const navigate = useNavigate();
@@ -131,44 +150,87 @@ const Register: React.FC = () => {
     email: "",
     password: "",
     passwordCheck: "",
-    phone: "",
     name: "",
     information: "",
   });
-  const [userInfo, setUserInfo] = useRecoilState<UserInfoType>(userInfoState);
+  const [isEmailChecked, setIsEmailChecked] = useState<boolean | null>(null);
+  const [isPasswordError, setIsPasswordError] = useState(false);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    if (name === "email") {
+      setIsEmailChecked(null);
+    }
   };
 
   const handleNext = (e: React.FormEvent) => {
     e.preventDefault();
+    if (!/^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{8,}$/.test(formData.password)) {
+      alert("비밀번호는 영문과 숫자를 포함한 8자 이상이어야 합니다.");
+      return;
+    }
+    if (formData.password !== formData.passwordCheck) {
+      setIsPasswordError(true);
+      return;
+    } else {
+      setIsPasswordError(false);
+    }
+    setIsPasswordError(false);
     setStep(step + 1);
   };
 
-  const handleBack = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setStep(step - 1);
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/users/register",
+        {
+          email: formData.email,
+          password: formData.password,
+          name: formData.name,
+          info: formData.information
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          }
+        }
+      );
+      if (response.status === 200) {
+        alert("회원가입이 완료되었습니다!");
+        navigate("/auth/login");
+      }
+    } catch (error) {
+      console.error("회원가입 중 오류가 발생했습니다.", error);
+      alert("회원가입 중 오류가 발생했습니다.");
+    }
+
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // 제출 로직 추가
-    console.log("Form Data:", formData);
-    setUserInfo({
-      ...userInfo,
-      email: formData.email,
-      photo: null,
-      userId: 1,
-      name: formData.name,
-      info: formData.information
-    });
+  const handleEmailCheck = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/users/email`, {
+        headers: {
+          Accept: "application/json",
+        },
+        params: {
+          email: formData.email,
+        },
+      });
 
-    localStorage.setItem("token", "test");
-    navigate("/");
+      if (response.data.result) {
+        alert("이미 사용중인 이메일입니다!");
+        setIsEmailChecked(false);
+      } else {
+        alert("사용가능한 이메일입니다.");
+        setIsEmailChecked(true);
+      }
+    } catch (error) {
+      console.error("There was an error checking the email!", error);
+    }
   };
 
   return (
@@ -182,7 +244,6 @@ const Register: React.FC = () => {
           <LeftSide>
             <Title>Garela</Title>
             <Subtitle>Best Community</Subtitle>
-            {/* 이미지 추가 가능 */}
           </LeftSide>
           <RightSide>
             {step === 1 && (
@@ -193,33 +254,39 @@ const Register: React.FC = () => {
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
+                  isChecked={isEmailChecked}
                   required
                 />
+                <Button type="button" onClick={handleEmailCheck}>
+                  Check Email
+                </Button>
                 <Label>Password</Label>
                 <Input
                   type="password"
                   name="password"
                   value={formData.password}
                   onChange={handleChange}
+                  isChecked={null}
+                  isError={isPasswordError}
                   required
                 />
+
                 <Label>Check Password</Label>
                 <Input
                   type="password"
                   name="passwordCheck"
                   value={formData.passwordCheck}
                   onChange={handleChange}
+                  isChecked={null}
+                  isError={isPasswordError}
                   required
                 />
-                <Label>Phone</Label>
-                <Input
-                  type="text"
-                  name="phone"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  required
-                />
-                <Button type="submit">Next</Button>
+                {isPasswordError && (
+                  <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+                )}
+                <Button type="submit" disabled={!isEmailChecked}>
+                  Next
+                </Button>
               </Form>
             )}
             {step === 2 && (
@@ -230,6 +297,7 @@ const Register: React.FC = () => {
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
+                  isChecked={null}
                   required
                 />
                 <Label>Information</Label>
@@ -239,9 +307,6 @@ const Register: React.FC = () => {
                   onChange={handleChange}
                   required
                 />
-                <Button type="button" onClick={handleBack}>
-                  Back
-                </Button>
                 <Button type="submit">Register</Button>
               </Form>
             )}
